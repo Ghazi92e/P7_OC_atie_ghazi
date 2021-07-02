68c19131032ca135230d4b13d46b691081db07a3
Ghazi <ghaziatie@gmail.com>
2021-07-02 02:38:03 +0200

    add mediawiki

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import json
-import urllib.request
-
+# coding=utf-8
 import requests
 from flask import Flask, request, redirect, url_for, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -8,15 +6,14 @@ from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
 
 import stop_words
-from parsedata import filterdata
 from stop_words import STOP_WORDS
 
-app = Flask(__name__)  # Crée une instance de la classe Flask, c'est notre application
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ghazi:Liban@localhost/todotest'  # Nom de la BDD
-db = SQLAlchemy(app)  # Lie l'app à SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ghazi:Liban@localhost/todotest'
+db = SQLAlchemy(app)
 
 
-class Task(db.Model):  # Modèle
+class Task(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.now)
@@ -28,22 +25,6 @@ bots = ['Salut cest papyGrantbot', 'cest le bot']
 @app.route('/')
 def index():
     return render_template('index.html')
-
-    """
-    name = request.form['parsetext']  # Récupérer les données envoyées par le form
-    proc = name
-    d = filterdata(proc)
-    test = geocode(d)
-    print(geocode(d))
-    return render_template('index.html', ok=name, data=test)
-    if name:
-    filterdata(name)
-    task = Task(name=name)  # Créer la tache dans la BDD avec le modèle Task
-    db.session.add(task)  # Ajout de notre objet à la session
-    db.session.commit()  # Enregistrer la tache dans la BDD
-    data = Task.query.all()
-    return render_template('index.html', data=data)
-"""
 
 
 @app.route('/testgooglemaps', methods=['GET', 'POST'])
@@ -81,6 +62,15 @@ def hello():
     return jsonify({'data': req})
 
 
+def filterdata(data_user):
+    filter_data = []
+    for data in data_user.split():
+        if data not in STOP_WORDS:
+            filter_data.append(data)
+    return filter_data
+
+
+"""
 @app.route('/filter', methods=['GET', 'POST'])
 def filterdataa():
     filter_data = []
@@ -90,34 +80,94 @@ def filterdataa():
         if name not in STOP_WORDS:
             filter_data.append(name)
     return jsonify(data=filter_data)
+"""
 
-
+"""
 @app.route('/bot', methods=['POST'])
 def bot():
-    name = request.form['botgeo']  # Récupérer les données envoyées par le form
+    name = request.form['botgeo']
     proc = name
     d = filterdata(proc)
     test = geocode(d)
     print(geocode(d))
     return render_template('bot.html', ok=name, data=test)
+"""
 
 
 def geocode(address):
     data = []
-    GOOGLE_MAPS_API_URL = f'https://maps.googleapis.com/maps/api/geocode/json?address={address}&key='
+    GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address={}/&key='.format(
+        address)
 
     req = requests.get(GOOGLE_MAPS_API_URL)
     d = req.json()
     data.append(d)
-    for datageo in data:
-        return datageo["results"][0]["formatted_address"]
+    if address:
+        for datageo in data:
+            return datageo["results"][0]["formatted_address"]
+
+
+def wikigeocode(address):
+    data = []
+    GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address={}/&key='.format(
+        address)
+
+    req = requests.get(GOOGLE_MAPS_API_URL)
+    d = req.json()
+    data.append(d)
+    if address:
+        for datageo in data:
+            return datageo['results'][0]['address_components'][0]['long_name']
 
 
 @app.route('/ajaxtest', methods=['POST'])
 def ajaxtest():
+    global address
+    global wikipageid
     data = request.form['address']
-    output = data
-    d = filterdata(output)
-    test = geocode(d)
-    print(test)
-    return jsonify({'output':test})
+    data_parse = filterdata(data)
+    address = geocode(data_parse)
+    wikiaddress = wikigeocode(data_parse)
+    datawiki = []
+    MEDIA_WIKI_API = "http://fr.wikipedia.org/w/api.php"
+    params = {
+        "format": "json",
+        "action": "query",
+        "prop": "extracts",
+        "explaintext": "",
+        "titles": f'{wikiaddress}'
+    }
+    data = requests.get(MEDIA_WIKI_API, params=params)
+    d = data.json()
+    datawiki.append(d)
+    for dw in datawiki:
+        idpage = dw['query']['pages']
+        for page_id in idpage:
+            wikipageid = page_id
+    for infodata in datawiki:
+        titleinfo = infodata['query']['pages'][wikipageid]['extract']
+        return jsonify({'output': address, 'wikidata': titleinfo})
+
+
+@app.route('/wikimedia')
+def wikimedia():
+    global wikipageid
+    datawiki = []
+    MEDIA_WIKI_API = "http://fr.wikipedia.org/w/api.php"
+    params = {
+        "format": "json",
+        "action": "query",
+        "prop": "extracts",
+        "explaintext": "",
+        "titles": "cité Paradis"
+    }
+    data = requests.get(MEDIA_WIKI_API, params=params)
+    d = data.json()
+    datawiki.append(d)
+    for dw in datawiki:
+        idpage = dw['query']['pages']
+        for page_id in idpage:
+            wikipageid = page_id
+    for infodata in datawiki:
+        titleinfo = infodata['query']['pages'][wikipageid]['extract']
+        return titleinfo
