2850efe0afb87b98cf8f9be9ffe4201e0c76a7c5
Ghazi <ghaziatie@gmail.com>
2021-07-10 00:52:44 +0200

    refactor

--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ from os import environ
 
 API_GOOGLEMAPS_KEY = environ.get('API_GOOGLEMAPS_KEY')
 
-is_prod = os.environ.get('IS_HEROKU', None)
+is_prod = os.environ.get('API_GOOGLEMAPS_KEY', None)
 if is_prod:
     API_GOOGLEMAPS_KEY = environ.get('API_GOOGLEMAPS_KEY')
